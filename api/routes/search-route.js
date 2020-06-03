14d0db3527e9c183890d5b802439d964d108dec4
KyawswarThant <kyawswar.thant25@gmail.com>
2020-06-03 22:18:26 +0630

    image

--- api/routes/search-route.js
+++ api/routes/search-route.js
@@ -2,30 +2,25 @@ const express=require("express");
 const bodyParser = require('body-parser'); 
 const mongoJs=require("mongojs");
 const urlencodedParser = bodyParser.urlencoded({ extended: false }) 
-const db=mongoJs('Kyawswar:Kyawswar@localhost/places',['information'])
+const db=mongoJs('places',['information','fs.files'])
 const router=express.Router();
 let response;
 router.use(bodyParser.json());
 router.get("/:place",urlencodedParser,function(req,res){
     response=req.params;
-    console.log(response.place);
+    console.log(req.params.place);
     db.information.find({place:response.place},function(err,result){
-        /*response=result.map(i=>{
-            return i.content;
-        })
-        console.log("hey")*/
+        console.log(result.place);
         res.status(200).send(JSON.stringify(result));
-    })
-    //res.redirect(`http://localhost:9000/search-route/${response}`);  
+    });
 });
 router.get("/",function(req,res){
-    db.information.find(function(err,result){
-        response=result.map(i=>{
-            return i.content;
-        })
-        console.log(JSON.stringify(response[0]));
-        console.log("hey")
-        res.status(200).send(JSON.stringify(result));
-    })
-})
+    db.fs.files.find(function(err,result){
+        if(err)
+        res.json(err);
+        else
+        console.log(result.uploadDate);
+        res.send(JSON.stringify(result.filename));
+    });
+});
 module.exports=router;
